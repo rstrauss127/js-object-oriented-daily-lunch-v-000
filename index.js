82da1cd26ac81e387ae8cdb953203b494209dc3e
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-22 16:23:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0;
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodId;
+  }
+}
