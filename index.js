82ba68eb97a1344b13f35942cc6a2ab0ee2749fa
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-22 16:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0;
 let customerId = 0;
+let mealId = 0;
 
 class Neighborhood {
   constructor(name) {
@@ -13,5 +14,14 @@ class Neighborhood {
 class Customer {
   constructor(name) {
     this.name = name;
+    this.id = ++customerId;
+  }
+}
+
+class Meal {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+    this.id = ++mealId;
   }
 }
